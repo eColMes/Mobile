8a93627a2ef173604c46bbbe5f64fb30d0a53b7e
YeXiaoMin <xkcyy@outlook.com>
2017-08-17 10:24:13 +0800

    v

---
+++ Mobile/demos/components/picker.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../../assets/mobile/js/plugins/knockout-3.3.0/knockout.js" />
+
+function picker(model) {
+    this = {
+        isMuilt:model.isMuilt||false,
+        title: ko.observable(""),
+        list: ko.observableArray([]),
+        selected: ko.observable({}),
+    }
+    return this;
+}
+
+
